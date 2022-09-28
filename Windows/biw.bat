239d00c6c900776ae961776948462df0ba4c1fdd
Omar Zeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-28 10:30:20 +0200

    Update biw.bat

--- Windows/biw.bat
+++ Windows/biw.bat
@@ -1,75 +1,183 @@
-ECHO ON
+#!/bin/bash 
+@ECHO OFF
+CLS 
+ECHO 1. Changing hosts file
+ECHO 2. Changing DNS Servers (CloudFlare)
+ECHO 3. Changing DNS Servers (Family Sheild)
 
 
-ECHO "Copying the original Windows Hosts file" 
 
+CHOICE /C 123 /M "Enter your choice:"
+
+
+
+:: Note - list ERRRORLEVELS in decreasing order
+IF ERRORLEVEL 3 GOTO Changing DNS Servers (CloudFlare)
+IF ERRORLEVEL 2 GOTO Changing DNS Servers (Family Sheild)
+IF ERRORLEVEL 1 GOTO Changing hosts file 
+
+
+:Changing hosts file 
+ECHO "Copying the original Windows Hosts file "
 CD C:\Users\Public\Documents
 MKDIR OriginalFile
 CD
 CD C:\Windows\System32\drivers\etc
 COPY hosts C:\Users\Public\Documents\OriginalFile
-DEL hosts
-CD 
+ECHO "Your original file was copied C:\Users\Public\Documents\OriginalFile " 
+DEL C:\Windows\System32\drivers\etc\hosts
 CD C:\Users\Administrator\Downloads\Block-Illegal-Websites-master\Block-Illegal-Websites-master\etc
 COPY hosts C:\Windows\System32\drivers\etc
 ipconfig /flushdns
+ECHO "Your Hosts File Was Changed SuccessFully" 
+GOTO END
+
+
+
+:Changing DNS Servers (Family Sheild) 
+    ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
+
+    ECHO "Changing DNS Servers to Clean-DNS ..."
+
+
+    ECHO  " Changing DNS Of This PC-Only Wifi....." 
+
+    netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
+
+    ECHO  " Changing DNS Of This PC-Only Ethernet ...."
+
+
+    netsh interface ipv4 set dns "Ethernet" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Ethernet" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Ethernet" 2a0d:2a00:2:: index=2
+
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+    netsh interface ipv4 set dns "Local Area Connection" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Local Area Connection" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Local Area Connection" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Local Area Connection" 2a0d:2a00:2:: index=2
+
+
+
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+
+    netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
+
+        
+;;
+
+
+:Changing DNS Servers (CloudFlare)
+ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
+
+    ECHO "Changing DNS Servers to Clean-DNS ..."
+
+
+    ECHO  " Changing DNS Of This PC-Only Wifi....." 
+
+    netsh interface ipv4 set dns "Wi-Fi" static 1.1.1.3 primary
+    netsh interface ipv4 add dns "Wi-Fi" 1.0.0.3 index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2606:4700:4700::1113
+    netsh interface ipv6 add dns "Wi-Fi" 2606:4700:4700::1003 index=2
+
+    ECHO  " Changing DNS Of This PC-Only Ethernet ...."
+
+
+    netsh interface ipv4 set dns "Ethernet" static 1.1.1.3 primary
+    netsh interface ipv4 add dns "Ethernet"  1.0.0.3 index=2
+    netsh interface ipv6 set dns "Ethernet" static 2606:4700:4700::1113 primary
+    netsh interface ipv6 add dns "Ethernet" 2606:4700:4700::1003 index=2
+
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+    netsh interface ipv4 set dns "Local Area Connection" static 1.1.1.3 primary
+    netsh interface ipv4 add dns "Local Area Connection" 1.0.0.3 index=2
+    netsh interface ipv6 set dns "Local Area Connection" static 2606:4700:4700::1113 primary
+    netsh interface ipv6 add dns "Local Area Connection" 2606:4700:4700::1003 index=2
+
+
+
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+
+    netsh interface ipv4 set dns "Wi-Fi" static 1.1.1.3  primary
+    netsh interface ipv4 add dns "Wi-Fi" 1.0.0.3  index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2606:4700:4700::1113 primary
+    netsh interface ipv6 add dns "Wi-Fi" static 2606:4700:4700::1113 primary
+;;
 
 
 
 
 
-ECHO "Changing DNS Servers to Clean-DNS ..."
-
-
-ECHO  " Changing DNS Of This PC-Only Wifi....." 
-
-netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
-netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
-netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
-netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
-
-ECHO  " Changing DNS Of This PC-Only Ethernet ...."
-
-
-netsh interface ipv4 set dns "Ethernet" static 185.228.168.168 primary
-netsh interface ipv4 add dns "Ethernet" 185.228.169.168 index=2
-netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1:: primary
-netsh interface ipv6 add dns "Ethernet" 2a0d:2a00:2:: index=2
-
-ECHO " Changing DNS Of This PC-Only Local Area Connection...."
-
-netsh interface ipv4 set dns "Local Area Connection" static 185.228.168.168 primary
-netsh interface ipv4 add dns "Local Area Connection" 185.228.169.168 index=2
-netsh interface ipv6 set dns "Local Area Connection" static 2a0d:2a00:1:: primary
-netsh interface ipv6 add dns "Local Area Connection" 2a0d:2a00:2:: index=2
 
 
 
-ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+esac
 
 
-netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
-netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
-netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
-netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
-
-	
+    ECHO "Changing DNS Servers to Clean-DNS ..."
 
 
+    ECHO  " Changing DNS Of This PC-Only Wifi....." 
 
-#Windows 7
+    netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
 
-#ECHO "Windows 7"
+    ECHO  " Changing DNS Of This PC-Only Ethernet ...."
 
-#Win7 Wifi
 
-#netsh interface ip set dns "Wi-Fi" static 185.228.168.168
-#netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1::
-	
+    netsh interface ipv4 set dns "Ethernet" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Ethernet" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Ethernet" 2a0d:2a00:2:: index=2
 
-# Win7 Ethernet
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
 
-#netsh interface ip set dns "Ethernet" static 185.228.168.168
-#netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1::	
-	
+    netsh interface ipv4 set dns "Local Area Connection" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Local Area Connection" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Local Area Connection" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Local Area Connection" 2a0d:2a00:2:: index=2
+
+
+
+    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
+
+
+    netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
+
+        
+
+
+
+    #Windows 7
+
+    #ECHO "Windows 7"
+
+    Win7 Wifi
+
+    netsh interface ip set dns "Wi-Fi" static 185.228.168.168
+    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1::
+        
+
+    Win7 Ethernet
+
+    netsh interface ip set dns "Ethernet" static 185.228.168.168
+    netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1::	
+        
 
