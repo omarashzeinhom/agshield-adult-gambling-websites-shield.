107a37c72a81137885b17d25034d4102d138fcf0
Omar Zeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-28 10:44:05 +0200

    Update biw.bat

--- Windows/biw.bat
+++ Windows/biw.bat
@@ -2,20 +2,18 @@
 @ECHO OFF
 CLS 
 ECHO 1. Changing hosts file
-ECHO 2. Changing DNS Servers (CloudFlare)    Win10
-ECHO 3. Changing DNS Servers (Family Sheild) Win10 
-ECHO 4. Changing DNS Servers (Family Sheild) Win 7
+ECHO 2. Changing DNS Servers (CloudFlare Family) Win10
+ECHO 3. Changing DNS Servers (Family Sheild) Win 7
 
 
 
-CHOICE /C 123 /M "Enter your choice:"
+CHOICE /C 1234 /M "Enter your choice:"
 
 
 
 :: Note - list ERRRORLEVELS in decreasing order
-IF ERRORLEVEL 4 GOTO Changing DNS Servers (CloudFlare) Windows 7
-IF ERRORLEVEL 3 GOTO Changing DNS Servers (CloudFlare)
-IF ERRORLEVEL 2 GOTO Changing DNS Servers (Family Sheild)
+IF ERRORLEVEL 3 GOTO Changing DNS Servers (CloudFlare) Windows 7
+IF ERRORLEVEL 2 GOTO Changing DNS Servers (CloudFlare Family) Win10
 IF ERRORLEVEL 1 GOTO Changing hosts file 
 
 
@@ -28,7 +26,6 @@ CD C:\Windows\System32\drivers\etc
 COPY hosts C:\Users\Public\Documents\OriginalFile
 ECHO "Your original file was copied C:\Users\Public\Documents\OriginalFile " 
 DEL C:\Windows\System32\drivers\etc\hosts
-CD C:\Users\Administrator\Downloads\Block-Illegal-Websites-master\Block-Illegal-Websites-master\etc
 COPY hosts C:\Windows\System32\drivers\etc
 ipconfig /flushdns
 ECHO "Your Hosts File Was Changed SuccessFully" 
@@ -36,7 +33,7 @@ GOTO END
 
 
 
-:Changing DNS Servers (Family Sheild) 
+:Changing Changing DNS Servers (CloudFlare Family) Win10
     ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
 
     ECHO "Changing DNS Servers to Clean-DNS ..."
@@ -78,47 +75,6 @@ GOTO END
 ;;
 
 
-:Changing DNS Servers (CloudFlare)
-ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
-
-    ECHO "Changing DNS Servers to Clean-DNS ..."
-
-
-    ECHO  " Changing DNS Of This PC-Only Wifi....." 
-
-    netsh interface ipv4 set dns "Wi-Fi" static 1.1.1.3 primary
-    netsh interface ipv4 add dns "Wi-Fi" 1.0.0.3 index=2
-    netsh interface ipv6 set dns "Wi-Fi" static 2606:4700:4700::1113
-    netsh interface ipv6 add dns "Wi-Fi" 2606:4700:4700::1003 index=2
-
-    ECHO  " Changing DNS Of This PC-Only Ethernet ...."
-
-
-    netsh interface ipv4 set dns "Ethernet" static 1.1.1.3 primary
-    netsh interface ipv4 add dns "Ethernet"  1.0.0.3 index=2
-    netsh interface ipv6 set dns "Ethernet" static 2606:4700:4700::1113 primary
-    netsh interface ipv6 add dns "Ethernet" 2606:4700:4700::1003 index=2
-
-    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
-
-    netsh interface ipv4 set dns "Local Area Connection" static 1.1.1.3 primary
-    netsh interface ipv4 add dns "Local Area Connection" 1.0.0.3 index=2
-    netsh interface ipv6 set dns "Local Area Connection" static 2606:4700:4700::1113 primary
-    netsh interface ipv6 add dns "Local Area Connection" 2606:4700:4700::1003 index=2
-
-
-
-    ECHO " Changing DNS Of This PC-Only Local Area Connection...."
-
-
-    netsh interface ipv4 set dns "Wi-Fi" static 1.1.1.3  primary
-    netsh interface ipv4 add dns "Wi-Fi" 1.0.0.3  index=2
-    netsh interface ipv6 set dns "Wi-Fi" static 2606:4700:4700::1113 primary
-    netsh interface ipv6 add dns "Wi-Fi" static 2606:4700:4700::1113 primary
-;;
-
-
-
 :Changing DNS Servers (CloudFlare) Windows 7
    ECHO "Windows 7 Ethernet"
    netsh interface ip set dns "Wi-Fi" static 1.1.1.3  primary
@@ -129,7 +85,7 @@ ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
    ECHO "Win7 Wifi"
    netsh interface ip set dns "Wi-Fi" static 1.1.1.3 primary
    netsh interface ip set dns "Wi-Fi" static 1.0.0.3 index=2
-    netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
+   netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
    netsh interface ipv6 set dns "Wi-Fi" 2606:4700:4700::1003 index=2
       
-
+;;
