6e41edb2cf8659ee01b18cd7dcdfad36ab3cce4f
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-29 08:43:45 +0200

    v1.0.3

--- Windows/blockag.bat
+++ Windows/blockag.bat
@@ -54,15 +54,15 @@ GOTO START
 :CleanBrowsing_Family_Win10
     ECHO "Changing Search Engine Results to Safe Family (CloudFlare Family DNS)."
     ECHO  " Changing DNS Of This PC-Only Wifi..............................................................................." 
+    netsh interface ipv4 set dns "Wired Ethernet Connection" static 185.228.168.168 primary
+    netsh interface ipv4 add dns "Wired Ethernet Connection" 185.228.169.168 index=2
+    netsh interface ipv6 set dns "Wired Ethernet Connection" static 2a0d:2a00:1:: primary
+    netsh interface ipv6 add dns "Wired Ethernet Connection" 2a0d:2a00:2:: index=2
+    ECHO  " Changing DNS Of This PC-Only Ethernet ..............................................................."
     netsh interface ipv4 set dns "Wi-Fi" static 185.228.168.168 primary
     netsh interface ipv4 add dns "Wi-Fi" 185.228.169.168 index=2
     netsh interface ipv6 set dns "Wi-Fi" static 2a0d:2a00:1:: primary
     netsh interface ipv6 add dns "Wi-Fi" 2a0d:2a00:2:: index=2
-    ECHO  " Changing DNS Of This PC-Only Ethernet ..............................................................."
-    netsh interface ipv4 set dns "Ethernet" static 185.228.168.168 primary
-    netsh interface ipv4 add dns "Ethernet" 185.228.169.168 index=2
-    netsh interface ipv6 set dns "Ethernet" static 2a0d:2a00:1:: primary
-    netsh interface ipv6 add dns "Ethernet" 2a0d:2a00:2:: index=2
     netsh interface ipv4 set dns "Local Area Connection" static 185.228.168.168 primary
     netsh interface ipv4 add dns "Local Area Connection" 185.228.169.168 index=2
     netsh interface ipv6 set dns "Local Area Connection" static 2a0d:2a00:1:: primary
@@ -109,10 +109,10 @@ GOTO START
     netsh interface ipv6 set dns "Wi-Fi" static 2606:4700:4700::1113 primary
     netsh interface ipv6 add dns "Wi-Fi" static 2606:4700:4700::1003 index=2
     ECHO  " Changing DNS Of This PC-Only Ethernet ..............................................................."
-    netsh interface ipv4 set dns "Ethernet" static 1.1.1.3 primary
-    netsh interface ipv4 add dns "Ethernet" static 1.0.0.3 index=2
-    netsh interface ipv6 set dns "Ethernet" static 2606:4700:4700::1113 primary
-    netsh interface ipv6 add dns "Ethernet" static 2606:4700:4700::1003 index=2
+    netsh interface ipv4 set dns "Wired Ethernet Connection" static 1.1.1.3 primary
+    netsh interface ipv4 add dns "Wired Ethernet Connection" static 1.0.0.3 index=2
+    netsh interface ipv6 set dns "Wired Ethernet Connection" static 2606:4700:4700::1113 primary
+    netsh interface ipv6 add dns "Wired Ethernet Connection" static 2606:4700:4700::1003 index=2
     ECHO " Changing DNS Of This PC-Only Local Area Connection.............................................."
     netsh interface ipv4 set dns "Local Area Connection" static 1.1.1.3 primary
     netsh interface ipv4 add dns "Local Area Connection" static 1.0.0.3 index=2
@@ -160,10 +160,10 @@ GOTO START
     netsh interface ipv6 set dns "Wi-Fi" static 2001:4860:4860::8888 primary
     netsh interface ipv6 add dns "Wi-Fi" static 2001:4860:4860::8844 index=2        
     ECHO  " Changing DNS Of This PC-Only Ethernet ..............................................................."
-    netsh interface ipv4 set dns "Ethernet" static 8.8.8.8 primary
-    netsh interface ipv4 add dns "Ethernet" static  static 8.8.4.4 index=2
-    netsh interface ipv6 set dns "Ethernet" static 2001:4860:4860::8888 primary
-    netsh interface ipv6 add dns "Ethernet" static 2001:4860:4860::8844 index=2  
+    netsh interface ipv4 set dns "Wired Ethernet Connection" static 8.8.8.8 primary
+    netsh interface ipv4 add dns "Wired Ethernet Connection" static  static 8.8.4.4 index=2
+    netsh interface ipv6 set dns "Wired Ethernet Connection" static 2001:4860:4860::8888 primary
+    netsh interface ipv6 add dns "Wired Ethernet Connection" static 2001:4860:4860::8844 index=2  
     ECHO  " Changing DNS Of This PC-Only Ethernet ..............................................................."
     netsh interface ipv4 set dns "Local Area Connection" static 8.8.8.8 primary
     netsh interface ipv4 add dns "Local Area Connection" static  static 8.8.4.4 index=2
@@ -204,9 +204,9 @@ GOTO END
 
 
 :Check_DNS_Servers 
-    IPCONFIG /ALL
-    ECHO "CHECKOUT THE NEW DNS SERVERS DNS Servers" 
-   PAUSE
+IPCONFIG /ALL
+ECHO "CHECKOUT THE NEW DNS SERVERS DNS Servers" 
+PAUSE
 
 ;
 GOTO END
